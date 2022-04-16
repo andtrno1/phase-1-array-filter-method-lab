43830194c65495f5851ac76d1d82583301dad155
andtrno1 <andtrno1@gmail.ccom>
2022-04-15 23:11:26 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function (driverNames) { return driverNames.toLowerCase() === name.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function (driverLetters) { return driverLetters.toLowerCase().slice(0,letters.length) === letters.toLowerCase()})
+}
+
+function matchName(drivers, match){
+    return drivers.filter(function (driverMatches) {return driverMatches.name === match})
+}
+
